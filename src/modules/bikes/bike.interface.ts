16d5779c765068b5d816f6d1bc544b9e58ab4602
mdrakibmia99 <rkrakibhasan680@gmail.com>
2024-11-22 01:28:06 +0600

    udpate bike interface

---
+++ src/modules/bikes/bike.interface.ts
@@ -0,0 +1,11 @@
+export interface IBike {
+  name: string;
+  brand: string;
+  price: number;
+  category: 'Mountain' | 'Road' | 'Hybrid' | 'Electric';
+  description: string;
+  quantity: number;
+  inStock: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
