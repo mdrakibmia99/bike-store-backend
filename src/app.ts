dafd31c732a9246306967c5ff893c2594e5dcdf0
mdrakibmia99 <rkrakibhasan680@gmail.com>
2024-11-22 02:21:58 +0600

    update root file

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import bikeRoutes from './modules/bikes/bike.route';
 import orderRoutes from './modules/orders/order.route';
@@ -14,5 +14,8 @@ app.use('/api/products', bikeRoutes);
 
 // this api order (get,post,update,delete)
 app.use('/api/orders', orderRoutes);
+app.get('/',(req:Request,res:Response)=>{
+    res.send("Welcome to bike store server...")
+})
 
 export default app;
